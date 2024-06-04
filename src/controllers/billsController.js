0c26bb1c15abd70207bb281895639da2bb428815
g-bolsoni <giovanebolsoni2@gmail.com>
2024-06-03 22:06:31 -0300

    Remove authenticate function

--- src/controllers/billsController.js
+++ src/controllers/billsController.js
@@ -4,8 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/usersModel');
 
 class productController {
-    async index(req, res) { //Listar constas
-        await this.authenticate(req, res, () => { }); // Autenticação
+    async index(req, res) { //Listar todas contas
 
         const bills = await billsModel.find({ user_id: req.user_id });
 
@@ -13,32 +12,12 @@ class productController {
 
     }
 
-    async authenticate(req, res, next) { // Função de autenticação para verificar o token e obter o user_id
-        const token = req.header('Authorization').replace('Bearer ', '');
-        if (!token) {
-            return res.status(401).json({ message: 'Autenticação necessária' });
-        }
-
-        try {
-            const decoded = jwt.verify(token, process.env.SECRET);
-            const user = await User.findById(decoded.id);
-            if (!user) {
-                throw new Error();
-            }
-            req.user_id = user._id;
-            next();
-        } catch (error) {
-            return res.status(401).json({ message: 'Token inválido' });
-        }
-    }
-
-    async findOne(req, res) { //Listar constas
-        await this.authenticate(req, res, () => { }); // Autenticação
+    async findOne(req, res) { //Listar uma conta
 
         try {
             const { id } = req.params;
 
-            const bill = await billsModel.findById(id);
+            const bill = await billsModel.findById({ _id: id, user_id: req.user_id });
 
             if (!bill) {
                 return res.status(404).json({ "message": 'Not Found' });
@@ -52,55 +31,66 @@ class productController {
     }
 
     async createBills(req, res) { //Criar conta
-        await this.authenticate(req, res, () => { }); // Autenticação
         try {
-            const createdbills = await billsModel.create(req.body);
+            const billData = req.body;
+            billData.user_id = req.user_id;
+
+            const createdbills = await billsModel.create(billData);
             return res.status(200).json(createdbills)
         } catch (error) {
             res.status(404).json({ "message": error });
         }
     }
 
-    async updateBills(req, res) { //atualiza conta
-        await this.authenticate(req, res, () => { }); // Autenticação
+    async updateBills(req, res) { //Atualizar conta
         const { id } = req.params;
         try {
-            await billsModel.findByIdAndUpdate(id, req.body);
-            return res.status(200).json({ "message": 'Item updated successfully' });
+            const bill = await billsModel.findOneAndUpdate(
+                { _id: id, user_id: req.user_id },
+                req.body,
+                { new: true }
+            );
+
+            if (!bill) {
+                return res.status(404).json({ message: 'This id not exists' });
+            }
+
+            return res.status(200).json({ message: 'Item updated successfully' });
         } catch (error) {
             return res.status(404).json({ "message": 'This id not exists' });
         }
     }
 
-    async deleteBills(req, res) { //Delete conta
-        await this.authenticate(req, res, () => { }); // Autenticação
+    async deleteBills(req, res) { //Deletar conta
         const { id } = req.params;
+
         try {
-            await billsModel.findByIdAndDelete(id);
+            const bill = await billsModel.findOneAndDelete({ _id: id, user_id: req.user_id }); // Garantir que a consulta inclua user_id
+
+            if (!bill) {
+                return res.status(404).json({ message: 'Bills not found' });
+            }
+            return res.status(200).json({ message: 'Bills successfully deleted' });
+        } catch (error) {
+            return res.status(404).json({ "message": "Bills not found" });
+        }
+    }
+
+    async deleteAllBills(req, res) { //Deletar conta
+        try {
+            billsModel.deleteMany({ user_id: req.user_id }); // Garantir que a consulta inclua user_id
             return res.status(200).json({ "message": "Bills successfully deleted" });
         } catch (error) {
             return res.status(404).json({ "message": "Bills not found" });
         }
     }
 
-    async deleteAllBills(req, res) { //Delete conta
-        await this.authenticate(req, res, () => { }); // Autenticação
-        try {
-            await billsModel.deleteMany();
-            return res.status(200).json({ "message": "Bills successfully deleted" });
-        } catch (error) {
-            return res.status(404).json({ "message": "Bills not found" });
-        }
-    }
-
-    async filterBills(req, res) { //Filtra os resultados
-        await this.authenticate(req, res, () => { }); // Autenticação
+    async filterBills(req, res) { //Filtrar resultados
 
         if (!req.body) { return res.status(404).json({ "message": "Not found" }) }
-        try {
-            const filter = await billsModel.find(req.body);
 
-            console.log(`filter -> ${filter}`);
+        try {
+            const filter = await billsModel.find({ ...req.body, user_id: req.user_id }); // Garantir que a consulta inclua user_id
             return res.status(200).json(filter);
         } catch (error) {
             return res.status(404).json({ "message": "Fileter failed" });
